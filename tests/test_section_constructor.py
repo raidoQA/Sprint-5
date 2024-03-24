6a9f05624d4864657e42dff3ed9fed7f4253e9ab
raidoQA <pwnd0@yandex.ru>
2024-03-24 13:09:30 +0300

    added attributes rework assert for sections constructor

--- tests/test_section_constructor.py
+++ tests/test_section_constructor.py
@@ -1,22 +1,27 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 from locators import Locators
-from const import Constants
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
 
 class TestSectionConstructor:
 
     def test_section_sauces(self, driver: WebDriver):
         sauces = driver.find_element(*Locators.sauces)
         sauces.click()
-        assert Constants.new_class in sauces.get_attribute("class")
+        WebDriverWait(driver, 2).until(expected_conditions.visibility_of_element_located(Locators.active_tab))
+        assert driver.find_element(*Locators.active_tab).text == 'Соусы'
 
     def test_section_fillings(self, driver: WebDriver):
         fillings = driver.find_element(*Locators.fillings)
         fillings.click()
-        assert Constants.new_class in fillings.get_attribute("class")
+        WebDriverWait(driver, 2).until(expected_conditions.visibility_of_element_located(Locators.active_tab))
+        assert driver.find_element(*Locators.active_tab).text == 'Начинки'
 
     def test_section_buns(self, driver: WebDriver):
         fillings = driver.find_element(*Locators.fillings)
         fillings.click()
         buns = driver.find_element(*Locators.buns)
         buns.click()
-        assert Constants.new_class in buns.get_attribute("class")
+        WebDriverWait(driver, 2).until(expected_conditions.visibility_of_element_located(Locators.active_tab))
+        assert driver.find_element(*Locators.active_tab).text == 'Булки'
